18ab98e5a862297e8283d21801dbadd73e54610c
LeeSeung-yeon <asysa6979@gmail.com>
2014-09-20 16:46:02 +0900

    특성 -> android -> appcompact 수정

---
+++ UIExample2/src/com/example/uiexample2/tempCoordinate.java
@@ -0,0 +1,12 @@
+package com.example.uiexample2;
+
+public class tempCoordinate {
+	
+	private int x, y;
+	
+	public tempCoordinate(){	}
+	
+	public int coordinateX(){	return 1;	}
+	
+	public int coordinateY(){	return 1;	}
+}
